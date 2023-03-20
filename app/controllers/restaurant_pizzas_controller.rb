bf7f697455c58161de1c0fa4dd3661502add9490
Lorraineken <lorrainekupa@gmail.com>
2023-03-20 17:40:18 +0300

    Added validation for price

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,21 @@
 class RestaurantPizzasController < ApplicationController
+
+rescue_from ActiveRecord::RecordInvalid, with: :create_error_message
+
+  def create 
+    restaurant_pizza = RestaurantPizza.create!(res_pizza_params)
+    render json:restaurant_pizza, status: :created
+
+end
+
+  private
+
+ def res_pizza_params 
+    params.permit(:price,:restaurant_id,:pizza_id)
+ end
+
+ def create_error_message 
+    render json: { errors: "validation errors"}, status: :unprocessable_entity
+ end
+
 end
