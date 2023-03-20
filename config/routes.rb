98e183ab3024411a6fcb79112200faca16742037
Lorraineken <lorrainekupa@gmail.com>
2023-03-20 15:21:34 +0300

    Updted pizzas route and serializer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :restaurant_pizzas
-  resources :pizzas
+  resources :pizzas, only: [:index]
   resources :restaurants, only: [:index,:show,:destroy]
 
 end
