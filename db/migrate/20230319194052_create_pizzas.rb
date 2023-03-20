f3622b40ba2e2b4adefb2f090edf8e6028327241
Lorraineken <lorrainekupa@gmail.com>
2023-03-20 10:21:36 +0300

    updated pizzas migration

--- db/migrate/20230319194052_create_pizzas.rb
+++ db/migrate/20230319194052_create_pizzas.rb
@@ -1,8 +1,8 @@
 class CreatePizzas < ActiveRecord::Migration[7.0]
   def change
     create_table :pizzas do |t|
-      t.string :name
-      t.text :ingredients
+      t.string :name, null:false
+      t.text :ingredients, null:false
 
       t.timestamps
     end
