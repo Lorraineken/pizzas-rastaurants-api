8d3223ec887e6f150825632ea411245ae9c33ac5
Lorraineken <lorrainekupa@gmail.com>
2023-03-20 10:19:01 +0300

    Updated restaurants columns

--- db/migrate/20230319194052_create_pizzas.rb
+++ db/migrate/20230319194052_create_pizzas.rb
@@ -2,7 +2,7 @@ class CreatePizzas < ActiveRecord::Migration[7.0]
   def change
     create_table :pizzas do |t|
       t.string :name
-      t.string :ingredients
+      t.text :ingredients
 
       t.timestamps
     end
