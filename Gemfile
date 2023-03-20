0c76558dcfda7d5425297f3d87f3cee66335dea1
Lorraineken <lorrainekupa@gmail.com>
2023-03-20 13:18:37 +0300

    Added seeds to seeds.rb

--- Gemfile
+++ Gemfile
@@ -18,6 +18,9 @@ gem "puma", "~> 5.0"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+# Faker
+gem 'faker', '~> 1.6', '>= 1.6.6'
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+4.times do 
+    restaurant_name = Faker::Restaurant.name
+    restaurant_address = Faker::Address.full_address
+    Restaurant.create!(name:restaurant_name,address:restaurant_address)
+end
+
+
+Pizza.create!(name:"Veggie",ingredients: Faker::Food.description)
+Pizza.create!(name:"Pepperoni",ingredients: Faker::Food.description) 
+Pizza.create!(name:"BBQ Chicken",ingredients: Faker::Food.description) 
+Pizza.create!(name:"Hawaiian",ingredients: Faker::Food.description)
+
+RestaurantPizza.create!(price:5,pizza_id:1,restaurant_id:1)
+RestaurantPizza.create!(price:10,pizza_id:1,restaurant_id:2)
+RestaurantPizza.create!(price:15,pizza_id:2,restaurant_id:3)
+RestaurantPizza.create!(price:25,pizza_id:3,restaurant_id:4)
+RestaurantPizza.create!(price:30,pizza_id:4,restaurant_id:5)
+RestaurantPizza.create!(price:5,pizza_id:2,restaurant_id:4)
+RestaurantPizza.create!(price:10,pizza_id:2,restaurant_id:5)
+RestaurantPizza.create!(price:15,pizza_id:1,restaurant_id:3)
+RestaurantPizza.create!(price:25,pizza_id:4,restaurant_id:3)
+RestaurantPizza.create!(price:30,pizza_id:1,restaurant_id:5)
