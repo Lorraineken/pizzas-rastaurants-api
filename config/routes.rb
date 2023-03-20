bbf74aa5d5acffd4a5e7c53ef8c5420b16565356
Lorraineken <lorrainekupa@gmail.com>
2023-03-20 14:00:02 +0300

    Added index action to restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   resources :restaurant_pizzas
   resources :pizzas
-  resources :restaurants
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index,:show,:destroy]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
